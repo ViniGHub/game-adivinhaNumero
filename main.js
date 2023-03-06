73a840f3c6cadd2b18ee78895138cfb084f47649
Vinicius Rodrigues da Silva <125066078+ViniGHub@users.noreply.github.com>
2023-03-06 12:34:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,30 +3,33 @@ let tentativas = 0;
 console.log(numVez);
 
 let balao = document.querySelector(".balao");
-let vd = true;
+let vd = false;
+let dv = true;
+
 
 function subForm() {
     chuteNum = document.querySelector("#advNum").value;
-    if (tentativas > 6 && chuteNum != numVez) {
-        vd = false;
+    if (tentativas > 6 && chuteNum != numVez && vd == false) {
+        dv = false;
         document.querySelector("#numTenta").innerHTML = `Você perdeu, fracassado!!! <br>O número era ${numVez}`;
         document.querySelector('.balao').classList.add('opacity-0');
         return;
     }
 
-    if (chuteNum > numVez) {
+    if (chuteNum > numVez && vd == false) {
         tentativas++;
         document.querySelector("#numTenta").innerHTML = `Você tentou advinhar ${tentativas} vezes`;
         document.querySelector("#numTenta").innerHTML += `<br>Muito alto`;
     }
 
-    if (chuteNum < numVez) {
+    if (chuteNum < numVez && vd == false) {
         tentativas++;
         document.querySelector("#numTenta").innerHTML = `Você tentou advinhar ${tentativas} vezes`;
         document.querySelector("#numTenta").innerHTML += `<br>Muito baixo`;
     }
 
-    if (chuteNum == numVez && vd == true) {
+    if (chuteNum == numVez && dv == true) {
+        vd = true;
         if (tentativas == 0) {
             tentativas++;
         }
